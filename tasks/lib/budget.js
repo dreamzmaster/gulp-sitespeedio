e9f13d96c13600c0433b2e36a1b80ec13815cbab
Ankit Singhal <asinghal@sapient.com>
2015-05-16 10:51:35 +0100

    First Draft

---
+++ tasks/lib/budget.js
@@ -0,0 +1,48 @@
+/**
+ * gulp-sitespeedio
+ * Released under the Apache 2.0 License
+ */
+'use strict';
+
+exports.checkBudget = function(data, gutil, config) {
+
+	// lets get the budget!
+	gutil.log(gutil.colors.blue('------------------------------------------------- Check budget'));
+
+	var failing = false;
+	var showFailedOnly = config.showFailedOnly;
+
+	var noPassedTests = 0;
+	var noFailingTests = 0;
+	var noSkippedTests = 0;
+
+	gutil.log(gutil.colors.green((showFailedOnly ? 'Will show only failing test.' : 'Show both failing and passing tests.') +
+		' Change this by set gulp config showFailedOnly to true'));
+
+	gutil.log(gutil.colors.cyan('the showFailedOnly is:' + showFailedOnly));
+
+	data.budget.forEach(function(result) {
+		if (result.skipped) {
+			noSkippedTests++;
+			if (!showFailedOnly)
+				gutil.log(gutil.colors.cyan('[SKIPPING]') + result.title + ' ' + result.url + ' ' + ' value [' + result.value +
+					']');
+		} else if (result.isOk) {
+			noPassedTests++;
+			if (!showFailedOnly) {
+				gutil.log(gutil.colors.green('[PASSED]') + ' The budget for ' + result.title + ' ' + result.url + '[' + result.value + ']');
+			}
+		} else {
+			noFailingTests++;
+			failing = true;
+			gutil.log(gutil.colors.red('[FAILED]') + ' The budget for ' + result.title + ' ' + result.url + ' failed. ' + result.description);
+		}
+	});
+
+	gutil.log('We got ' + noPassedTests + ' passing tests, ' + gutil.colors.red(noFailingTests + ' failing') + ((
+		noSkippedTests > 0) ? ' ' + noSkippedTests + ' skipped tests' : '.'));
+
+	gutil.log(gutil.colors.blue('------------------------------------------------- Finished checking budget'));
+
+	return failing;
+}
---
+++ tasks/sitespeed.js
@@ -0,0 +1,91 @@
+/*
+ * gulp-sitespeedio
+ * Released under the Apache 2.0 License
+ */
+
+'use strict';
+
+var gulp = require('gulp'),
+	Sitespeed = require('sitespeed.io/lib/sitespeed'),
+	fs = require('fs'),
+	path = require('path'),
+	gutil = require('gulp-util'),
+	budget = require('./lib/budget'),
+	assign = require('object-assign'),
+	tmp = require('temporary'),
+	PluginError = gutil.PluginError;
+
+
+var PLUGIN_NAME = 'gulp-sitespeedio';
+
+var gulpSitespeedio = function(options) {
+
+	console.log('Analyze your sites web performance');
+
+	if (!options.url) {
+		throw new PluginError('gulp-sitespeedio', 'Missing url option to Analyse');
+	}
+
+	var dir = new tmp.Dir(),
+		config = {
+			resultBaseDir: dir.path,
+			html: true,
+            showFailedOnly: false
+		};
+
+	assign(config, options);
+
+	// special handling for reading file with urls
+	// in sitespeed.io we read the file within the cli,
+	// so we need to do it from outside when we run without the
+	// cli
+	if (config.file) {
+		readFile(config);
+	}
+
+	return function(cb) {
+
+
+		var sitespeed = new Sitespeed(),
+			build = this;
+
+		sitespeed.run(options, function(err, data) {
+
+			if (err) {
+				cb(new gutil.PluginError('gulp-sitespeedio', err + '\n\n'));
+				
+			} else if (data && data.budget) {
+
+				var isFailing = budget.checkBudget(data, gutil, config);
+
+                if( isFailing ) {
+                    cb(new gutil.PluginError('gulp-sitespeedio', 'FAILED BUDGETS'));   
+                }
+
+			} else {
+                cb();
+            }
+		});
+	}
+
+};
+
+function readFile(options) {
+
+	// absoulute or relative
+	var fullPathToFile = (options.file.charAt(0) === path.sep) ? options.file : path.join(process.cwd(),
+		path.sep, options.file);
+
+	var data = fs.readFileSync(fullPathToFile);
+	var urls = data.toString().split(EOL);
+	urls = urls.filter(function(l) {
+		return l.length > 0;
+	});
+    
+	// we clean the file in the config to make
+	// it look that we are feeding with URL array
+	options.urls = urls;
+	options.file = undefined;
+}
+
+module.exports = gulpSitespeedio;
